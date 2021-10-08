7acd8fea5d273251ff157d258f424c5539e12b02
Roisin-13 <roisin.wheeler@gmail.com>
2021-10-08 17:02:18 +0100

    Sales class added in SalesApp namespace to contsruct Sale object

---
+++ SalesApp/SalesApp/SalesApp/Sale.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesApp.SalesApp
+{
+    class Sale
+    {
+        public int ID { get; set; }
+        private string name;
+        private int quantity;
+        private double price;
+        private DateTime date = DateTime.Now; //default date set for now
+
+        //--------get/set sale name
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        //--------get/set sale quantity
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+        //--------get/set sale price
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        //--------get/set sale date
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+        
+        //-----sale construcors:
+        public Sale(string name, int quantity, double price, DateTime date)
+        {
+            this.Name = name;
+            this.Quantity = quantity;
+            this.Price = price;
+            this.Date = date;
+        }
+
+        public override string ToString()
+        {
+            return $"Sale ID: {ID}, Name: {name}, Quantity: {quantity}, Price: {price}";
+        }
+        public Sale()
+        {
+        }
+
+       
+
+
+
+
+
+ 
+    }
+}
