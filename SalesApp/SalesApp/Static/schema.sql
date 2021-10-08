c994429b35ebde3318101115dbdab588f8fa184c
Roisin-13 <roisin.wheeler@gmail.com>
2021-10-08 19:51:18 +0100

    sql achema added

---
+++ SalesApp/SalesApp/Static/schema.sql
@@ -0,0 +1,8 @@
+create table if not exists sales (
+id int auto_increment,
+name varchar (150) not null,
+quantity int not null,
+price double null,
+date_of_sale datetime not null,
+primary key(id)
+);
