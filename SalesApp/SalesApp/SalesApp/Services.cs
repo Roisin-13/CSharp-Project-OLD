f15d4602dc32dded488998bbc958b9a0bc1dea63
Roisin-13 <roisin.wheeler@gmail.com>
2021-10-08 17:12:24 +0100

    services added for the controller method that are present

--- SalesApp/SalesApp/SalesApp/Services.cs
+++ SalesApp/SalesApp/SalesApp/Services.cs
@@ -7,6 +7,31 @@ using System.Threading.Tasks;
 namespace SalesApp.SalesApp
 {
     class Services
-    {
+    {      
+        //------linking repository with services
+        private readonly Repository repository;
+        public Services(Repository repository)
+        {
+            this.repository = repository;
+        }
+
+        //===========DATA ENTRY============//
+        internal Sale Create(Sale toCreate)
+        {
+            Sale newSale = repository.Create(toCreate);
+            return newSale;
+
+        }
+
+
+        //===========DELETE============//
+        internal void Delete(int id)
+        {
+            if (!repository.Exists(id))
+            {
+                throw new ItemNotFoundException();
+            }
+            repository.Delete(id);
+        }
     }
 }
